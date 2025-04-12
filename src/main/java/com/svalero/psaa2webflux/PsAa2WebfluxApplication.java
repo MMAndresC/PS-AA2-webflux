d47116982b7181d439c52294a899eebd69d899b1
Mar <mandrescasabon@hotmail.com>
2025-04-12 14:05:07 +0200

    :tada: init commit

---
+++ src/main/java/com/svalero/psaa2webflux/PsAa2WebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.psaa2webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsAa2WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsAa2WebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svalero/psaa2webflux/PsAa2WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.psaa2webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsAa2WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
